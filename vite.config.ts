877d48dc3f6255d1f3384e11b1a354a687e4bd34
Bumble <bumble.sakh@gmail.com>
2023-06-12 22:30:01 +1100

    init: start project

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: [
+      { find: '@components', replacement: path.resolve(__dirname, './src/components') },
+      { find: '@assets', replacement: path.resolve(__dirname, './src/assets') },
+      { find: '@styles', replacement: path.resolve(__dirname, './src/styles') },
+      { find: '@pages', replacement: path.resolve(__dirname, './src/pages') },
+      { find: '@configs', replacement: path.resolve(__dirname, './src/configs') },
+      { find: '@store', replacement: path.resolve(__dirname, './src/store') },
+    ],
+  },
+});
