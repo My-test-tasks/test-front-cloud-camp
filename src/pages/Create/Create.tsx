32ddf113cf2bfd3c89e26bbc316322e69b35f9f7
Bumble <bumble.sakh@gmail.com>
2023-06-18 15:19:21 +1100

    refactor: import

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,14 +1,14 @@
 import { StepNumber } from '@store/form/types';
 import styles from './Create.module.scss';
 import Form from './Form';
-import Header from './Stepper';
+import Stepper from './Stepper';
 
 const Create = () => {
   const steps = [StepNumber.one, StepNumber.two, StepNumber.three];
 
   return (
     <div className={styles.page}>
-      <Header steps={steps} />
+      <Stepper steps={steps} />
 
       <Form />
     </div>
